2e61036cea94d86ee0c21e5343182c02ea5feb71
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-12-03 00:18:42 -0300

    Restringindo quantidade de conta consumer por usuario

--- src/main/java/com/ayty/fintech/services/ConsumerService.java
+++ src/main/java/com/ayty/fintech/services/ConsumerService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ayty.fintech.domain.Consumer;
+import com.ayty.fintech.domain.User;
 import com.ayty.fintech.dtos.ConsumerDTO;
 import com.ayty.fintech.repositories.ConsumerRepository;
+import com.ayty.fintech.services.exceptions.InvalidRequestException;
 
 @Service
 public class ConsumerService {
@@ -13,8 +15,22 @@ public class ConsumerService {
 	@Autowired
 	private ConsumerRepository consumerRepository;
 	
+	@Autowired
+	private UserService userService;
+	
 	public Consumer insert(Consumer obj) {
-		return consumerRepository.save(obj);
+		
+		User user = userService.find(obj.getUserId());
+		
+		if (user.getConsumer() == null) {
+			obj = consumerRepository.save(obj);
+			user.setConsumer(obj);
+			userService.update(user);
+			return obj;
+		}
+		
+		throw new InvalidRequestException("Invalid Request!");
+		
 	}
 	
 	public Consumer fromDto(ConsumerDTO objDto) {
